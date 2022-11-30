ebaf84a30d15ec26b18e7aecd15b0868a060748c
JohnA <johngold01@msn.com>
2022-11-29 19:02:21 -0500

    updted files to see stock data populate in browser

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
-  $('#searchFilla').on('keyup',function(e){
-   let information = e.target.value;
+  $('#searchStocks').on('keypress',function(e){
+    if (e.which==13){
+      let information = e.target.value;
 
-    //Make request to API
-    $.ajax({
-      url: 'https://api.stockdata.org/v1/data/quote?symbols='+information+'&api_token=pAU9ED3DrAdzl26R0HQzXGokCoIZjmbu41FLJZuJ',
-      /*data: {
-        sources:information,
-        access_key: 'e7a211e54fe4a9c68b6dcbfecd183933',
-        }*/
-
-    }).done(function(response) {
-      console.log(response.data);
-    });
+      //Make ajax request to sotckdata.org Api
+      $.ajax({
+        url: 'https://api.stockdata.org/v1/data/quote?symbols='+information+'&api_token=pAU9ED3DrAdzl26R0HQzXGokCoIZjmbu41FLJZuJ',
+        /*data: {
+          sources:information,
+          access_key: 'e7a211e54fe4a9c68b6dcbfecd183933',
+          }*/
+  
+      }).done(function(response) {
+        console.log(response.data);
+        let data=response.data[0]
+        let dayHigh= "<div class = 'block'>"+data.day_high+"</div>"
+        $('#profile_results').html(dayHigh)
+      });
+    }
+   
   });
   });
 
