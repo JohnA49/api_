404c3a824459725a698fc87e5c8140310d763a31
JohnA <johngold01@msn.com>
2022-12-02 14:05:14 -0500

    file updated

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-  $('#searchStocks').on('keypress',function(e){
-    if (e.which==13){
-      let information = e.target.value;
+ $('#searchStocks').on('keypress',function(e){
+  if (e.which==13){
+    let information=e.target.value;
 
-      //Make ajax request to sotckdata.org Api
-      $.ajax({
-        url: 'https://api.stockdata.org/v1/data/quote?symbols='+information+'&api_token=pAU9ED3DrAdzl26R0HQzXGokCoIZjmbu41FLJZuJ',
-        /*data: {
-          sources:information,
-          access_key: 'e7a211e54fe4a9c68b6dcbfecd183933',
-          }*/
-  
-      }).done(function(response) {
-        console.log(response.data);
-        let data=response.data[0]
-        let dayHigh= "<div class = 'block'>"+data.day_high+"</div>"
-        $('#profile_results').html(dayHigh)
-      });
-    }
+    //make ajax requst to api-stockdata.org
+     $.ajax({
+        url:'https://api.stockdata.org/v1/data/quote?symbols='+information,
+        data:{
+         api_token:'pAU9ED3DrAdzl26R0HQzXGokCoIZjmbu41FLJZuJ',
+         limit:'50'
+        }
+        
+     }).done(function(stockdata){
+        console.log(stockdata.data);
+        let data=stockdata.data[0]
+        let responseObject= "<div class = 'block'>"+data.currency+"</div>"
+        $('#stockdata_results').html(responseObject)
+     });
+  }
    
-  });
-  });
+ });
+});
 
 
 
