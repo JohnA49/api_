6dc10eeff9a9bb3e3b2edaaf4571b23f7c8c7568
JohnA <johngold01@msn.com>
2022-12-04 12:22:44 -0500

    working on html dom

--- js/main.js
+++ js/main.js
@@ -13,17 +13,61 @@ $(document).ready(function(){
         
      }).done(function(stockdata){
         console.log(stockdata.data);
-        let data=stockdata.data[0]
-        let responseObject= "<div class = 'block'>"+data.currency+"</div>"
-        $('#stockdata_results').html(responseObject)
+        const data=stockdata.data[0];
+        const {ticker,price}=data;
+        //console.log(ticker);
+        //console.log(price); 
+        $('#stockdata_results').html(`
+         ${data.ticker}    ${data.price}
+      `)//correct
      });
-  }
-   
+
+  } 
  });
 });
 
+//utilizing the fetclh keyword...
+//console.log('utilize fetch function');
+
+catchCowrie()
+  .then(response=>{
+    //console.log('hurray');
+  })
+
+.catch(error=>{
+     console.log('error');
+     console.error(error);
+});
+async function catchCowrie(){
+  const response= await fetch('cowrie.jpg');
+  const blob= await response.blob();
+  document.getElementById('cowrie').src=URL.createObjectURL(blob); 
+}
+
+/* fetch('cowrie.jpg')
+  .then(response=>{
+    console.log(response);
+    return response.blob();
+   })
+   .then(blob=>{
+     console.log(blob);
+     document.getElementById('cowrie').src=URL.createObjectURL(blob);   
+  })
+   .catch(error=>{
+     console.log('error');
+     console.error(error);
+   });*/
 
 
+//utilzing async and await keywords
+ /*
+        const api_url= 'https://api.stockdata.org/v1/data/quote?symbols=AAPL%2CTSLA%2CMSFT&api_token=pAU9ED3DrAdzl26R0HQzXGokCoIZjmbu41FLJZuJ'
+        async function getStockData(){
+         const response= await fetch(api_url);
+         const data= await response.json();
+         console.log(data);
+        }
 
+        getStockData();*/
 
 
